b587d39a3c2b79cef6edbe7a8034e3093c811036
Sanat Sharma <sanatcolumban@gmail.com>
2017-07-24 18:25:53 -0700

    Added working camera UI

--- app/src/main/java/com/example/t_sashar/persona/ScanCamera.java
+++ app/src/main/java/com/example/t_sashar/persona/ScanCamera.java
@@ -1,18 +1,31 @@
 package com.example.t_sashar.persona;
 
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class ScanCamera extends AppCompatActivity {
+public class ScanCamera extends AppCompatActivity implements SurfaceHolder.Callback{
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -30,7 +43,6 @@ public class ScanCamera extends AppCompatActivity {
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
     private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
@@ -58,30 +70,14 @@ public class ScanCamera extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
+            mControlsView.setVisibility(View.GONE);
         }
     };
     private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    boolean mPreviewRunning;
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+    Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,30 +89,54 @@ public class ScanCamera extends AppCompatActivity {
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
+        getSupportActionBar().hide();
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.camera);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 50);
+
+        if(mCamera != null) {
+            mCamera.release();
+            Log.v("HERRE", "reached here");
+        }
+        mCamera = Camera.open();
+
+        Log.v("Camera opened",mCamera.getParameters().getFlashMode());
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    private Thread cameraThread = null;
+    private void openCamera(){
+        if(cameraThread == null) cameraThread = new Thread() {
+            @Override
+            public void run() {
+                openHelper();
+            }
+        };
+        cameraThread.start();
+        cameraThread.run();
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
     }
+    private void openHelper(){
+        try{
+            mCamera = Camera.open();
+            Log.v("CAMERA", "Camera opened??");
+        }
+        catch (RuntimeException e) {
+            Log.e("CAMERA LOG", "failed to open camera");
+        }
+    }
+
+
 
     private void toggle() {
         if (mVisible) {
@@ -152,6 +172,141 @@ public class ScanCamera extends AppCompatActivity {
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w,
+                               int h) {
+        if (mPreviewRunning) {
+            //  mCamera.stopPreview();
+        }
+        Camera.Parameters p = mCamera.getParameters();
+        List<Camera.Size> previewSizes = p.getSupportedPreviewSizes();
+        p.set("orientation", "portrait");
+
+        Camera.Size prevSize = getOptimalPreviewSize(previewSizes, w, h);
+
+        Camera.Size  previewSize = previewSizes.get(previewSizes.size()-1);
+        // p.setPreviewSize(previewSize.width, previewSize.height);
+        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+        if(display.getRotation() == Surface.ROTATION_0) {
+            mCamera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270){
+            mCamera.setDisplayOrientation(180);
+        }
+        p.setPreviewSize(prevSize.width, prevSize.height);
+        mCamera.setParameters(p);
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mCamera.startPreview();
+        mPreviewRunning = true;
+
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.2;
+        double targetRatio = (double) w / h;
+        if (sizes == null)
+            return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Camera.Size size : sizes) {
+            //Log.d(TAG, "Checking size " + size.width + "w " + size.height
+            //      + "h");
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+                continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the
+        // requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // mCamera.stopPreview();
+        mPreviewRunning = false;
+        if(mCamera != null) {
+            Log.v("Surface Destroyed", "INSIDE SURFACE DESTROYED");
+            mCamera.release();
+            // mCamera = null;
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        if (mCamera != null) {
+            Log.v("back press", "Inside back pressed");
+            mCamera.release();
+            //  mCamera = null;
+        }
+    }
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
+
+
     /**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
@@ -159,5 +314,42 @@ public class ScanCamera extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    @Override
+    public void onStop(){
+        if(mCamera != null) {
+            Log.v("Stop", "INSIDE STOP");
+            mCamera.release();
+            //mCamera = null;
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.v("Restart", "INside OnRestart");
+        if (mCamera == null)
+            mCamera.open();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.v("Resume", "INside OnResume");
+        if (mCamera == null)
+            mCamera.open();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(mCamera != null) {
+            Log.v("ONPAUSE", "INside on Pause");
+            // mCamera.release();
+            // mCamera = null;
+        }
+
     }
 }
